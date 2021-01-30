f197d2ff00877ac2b8d4059a4b73c0270480406d
Sivapreethi Ramesh <charmsivaramesh@gmail.com>
2021-01-29 21:19:16 -0800

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+const element = (
+    <div title="Introduction">
+    <h1 id="name">Sivapreethi Ramesh</h1><br></br>
+    <br></br>
+    <img src="/headshot.jpg" height="200" alt="photo"></img>
+    <br></br>
+    <br></br>
+    <div id="description">
+        I am a prospective graduate student currently doing my extended studies in Web and Mobile Application Development at SDSU. 
+        <br/><br/>
+        I have strong interests in developing robust and elegant Software Applications that solve specific user needs. 
+        <br/><br/>
+        I have profession work experience as a Software Engineer at Accenture. 
+        I am also a kickboxer and frequent hiker. 
+    </div>
+    <br></br>
+    <br></br>
+    <a href="https://google.com">
+        <button type="button">View my Github Repository</button>
+    </a>
+    </div>
+    
+);
+ReactDOM.render(element, document.getElementById('contents'));
